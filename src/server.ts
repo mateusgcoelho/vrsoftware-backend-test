b0ca9a61bcb39acbd9cc0a4a765224483182c8d3
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2022-02-14 14:26:42 -0300

    Adding initDefaultDatabase function!

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import { app } from "./app";
 
+import { initDefaultDatabase } from "./services/init_default_database";
+
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
-app.listen(SERVER_PORT, () => {
+app.listen(SERVER_PORT, async () => {
+  await initDefaultDatabase();
+
   console.log(`Server is running in ${SERVER_PORT}`);
 });
