d009b7601331c108a5be7956c2a4e612ad1e1479
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2022-02-17 12:24:26 -0300

    .

--- src/services/init_default_database.ts
+++ src/services/init_default_database.ts
@@ -1,16 +1,19 @@
 import { prisma } from "../database/client";
 
 const initDefaultDatabase = async () => {
-  await prisma.course.findMany().then(async (courses) => {
-    if (courses.length <= 0)
-      await prisma.course.create({
-        data: {
-          code: 1,
-          description: "INFORMATICA BASICA",
-          ementa: "Descrição padrão e breve do curso!",
-        },
-      });
-  });
+  await prisma.course
+    .findMany()
+    .then(async (courses) => {
+      if (courses.length <= 0)
+        await prisma.course.create({
+          data: {
+            code: 1,
+            description: "INFORMATICA BASICA",
+            ementa: "Descrição padrão e breve do curso!",
+          },
+        });
+    })
+    .finally(() => prisma.$disconnect());
 
   await prisma.student
     .findMany()
